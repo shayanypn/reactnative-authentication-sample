fd768782f1a0760102b14e72326811ee68b2a29b
shayanypn <shayanypn@gmail.com>
2020-08-19 23:37:18 +0430

    add gradient to welcome screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,29 +1,37 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import AppButton from "../components/Button";
 import Screen from "../components/Screen";
 import kstyles from "../kstyles";
 
 const WelcomeScreen = () => (
-  <Screen style={kstyles.bgGradient}>
-    <View style={styles.head}>
-      <Text style={styles.title}>YOUR PERSONAL TRAINER, RETHOUGHT.</Text>
-      <Text style={styles.description}>
-        Your everyday life is stressful enough.Stay flexible with online support
-        in everyday life.
-      </Text>
-    </View>
-    <View style={styles.foot}>
-      <AppButton title="Sign In with Apple" icon="apple" />
-      <AppButton
-        title="Sign In with Google"
-        bgColor={kstyles.primary}
-        icon="google"
-      />
-      <AppButton title="Sign Up with Email" bgColor={kstyles.green} />
-      <AppButton title="Log In" bgColor={kstyles.secondary} />
-      <Text style={styles.bottomText}>Explore the classes</Text>
-    </View>
+  <Screen style={kstyles.bgDarkBlue}>
+    <LinearGradient
+      colors={["rgba(0, 0, 0, 0)", "#000000"]}
+      style={{
+        flex: 1,
+      }}
+    >
+      <View style={styles.head}>
+        <Text style={styles.title}>YOUR PERSONAL TRAINER, RETHOUGHT.</Text>
+        <Text style={styles.description}>
+          Your everyday life is stressful enough.Stay flexible with online
+          support in everyday life.
+        </Text>
+      </View>
+      <View style={styles.foot}>
+        <AppButton title="Sign In with Apple" icon="apple" />
+        <AppButton
+          title="Sign In with Google"
+          bgColor={kstyles.primary}
+          icon="google"
+        />
+        <AppButton title="Sign Up with Email" bgColor={kstyles.green} />
+        <AppButton title="Log In" bgColor={kstyles.secondary} />
+        <Text style={styles.bottomText}>Explore the classes</Text>
+      </View>
+    </LinearGradient>
   </Screen>
 );
 
