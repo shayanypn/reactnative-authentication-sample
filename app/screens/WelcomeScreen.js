6d2557e8a0c6e0eb5d79e0cc83276857162a20d2
shayanypn <shayanypn@gmail.com>
2020-08-21 07:27:01 +0430

    trying to add apple sign in

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,43 +1,72 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+
+import * as Crypto from "expo-crypto";
+import * as AppleAuthentication from "expo-apple-authentication";
+
 import AppButton from "../components/Button";
 import Screen from "../components/Screen";
 import kstyles from "../kstyles";
 
-const WelcomeScreen = ({ navigation }) => (
-  <Screen style={kstyles.bgDarkBlue}>
-    <LinearGradient
-      colors={["rgba(0, 0, 0, 0)", "#000000"]}
-      style={{
-        flex: 1,
-      }}
-    >
-      <View style={styles.head}>
-        <Text style={styles.title}>YOUR PERSONAL TRAINER, RETHOUGHT.</Text>
-        <Text style={styles.description}>
-          Your everyday life is stressful enough.Stay flexible with online
-          support in everyday life.
-        </Text>
-      </View>
-      <View style={styles.foot}>
-        <AppButton title="Sign In with Apple" icon="apple" />
-        <AppButton
-          title="Sign In with Google"
-          bgColor={kstyles.primary}
-          icon="google"
-        />
-        <AppButton title="Sign Up with Email" bgColor={kstyles.green} />
-        <AppButton
-          title="Log In"
-          bgColor={kstyles.secondary}
-          onPress={() => navigation.navigate("Signin")}
-        />
-        <Text style={styles.bottomText}>Explore the classes</Text>
-      </View>
-    </LinearGradient>
-  </Screen>
-);
+const WelcomeScreen = ({ navigation }) => {
+  const loginWithApple = async () => {
+    const csrf = Math.random().toString(36).substring(2, 15);
+    const nonce = Math.random().toString(36).substring(2, 10);
+    const hashedNonce = await Crypto.digestStringAsync(
+      Crypto.CryptoDigestAlgorithm.SHA256,
+      nonce
+    );
+    const appleCredential = await AppleAuthentication.signInAsync({
+      requestedScopes: [
+        AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+        AppleAuthentication.AppleAuthenticationScope.EMAIL,
+      ],
+      state: csrf,
+      nonce: hashedNonce,
+    });
+    const { identityToken, email, state } = appleCredential;
+    console.log(identityToken, email, state);
+  };
+
+  return (
+    <Screen style={kstyles.bgDarkBlue}>
+      <LinearGradient
+        colors={["rgba(0, 0, 0, 0)", "#000000"]}
+        style={{
+          flex: 1,
+        }}
+      >
+        <View style={styles.head}>
+          <Text style={styles.title}>YOUR PERSONAL TRAINER, RETHOUGHT.</Text>
+          <Text style={styles.description}>
+            Your everyday life is stressful enough.Stay flexible with online
+            support in everyday life.
+          </Text>
+        </View>
+        <View style={styles.foot}>
+          <AppButton
+            title="Sign In with Apple"
+            icon="apple"
+            onPress={loginWithApple}
+          />
+          <AppButton
+            title="Sign In with Google"
+            bgColor={kstyles.primary}
+            icon="google"
+          />
+          <AppButton title="Sign Up with Email" bgColor={kstyles.green} />
+          <AppButton
+            title="Log In"
+            bgColor={kstyles.secondary}
+            onPress={() => navigation.navigate("Signin")}
+          />
+          <Text style={styles.bottomText}>Explore the classes</Text>
+        </View>
+      </LinearGradient>
+    </Screen>
+  );
+};
 
 const styles = StyleSheet.create({
   head: {
